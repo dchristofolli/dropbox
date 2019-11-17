0028c7b12d1ba5e557bf117595e187ee2077a496
dchristofolli <danielchristofolli@gmail.com>
2019-11-17 12:46:27 -0300

    tentativa de substituir o mongo pelo postgresql

--- impl/src/main/java/com/github/dchristofolli/dropbox/v1/file/FileFacade.java
+++ impl/src/main/java/com/github/dchristofolli/dropbox/v1/file/FileFacade.java
@@ -1,42 +1,42 @@
-package com.github.dchristofolli.dropbox.v1.file;
-
-import com.github.dchristofolli.dropbox.v1.file.model.FileMapper;
-import com.github.dchristofolli.dropbox.v1.file.model.FileModelRequest;
-import com.github.dchristofolli.dropbox.v1.file.service.FileService;
-import com.github.dchristofolli.dropbox.v1.user.service.UserService;
-import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
-
-@Component
-@AllArgsConstructor
-public class FileFacade {
-    private FileService fileService;
-    private UserService userService;
-
-    public void sendFile(MultipartFile file, String userId) {
-        fileService.send(file, userService.findById(userId));
-    }
-
-    public void delete(String idUser, String fileName) {
-        fileService.delete(userService.findById(idUser), fileName);
-    }
-
-    public Page<FileMapper> pagedList(FileModelRequest request) {
-        return fileService.pagedList(request);
-    }
-
-    public Page<FileMapper> listsSharedWithMe(FileModelRequest request) {
-        return fileService.listsSharedWithMe(request);
-    }
-
-    public void download(String id, String file) {
-        fileService.download(id, file);
-    }
-
-    public void downloadSharedWithMe(String id, String file) {
-        download(userService.findById(id).getFollower(), file);
-    }
-
-}
+//package com.github.dchristofolli.dropbox.v1.file;
+//
+//import com.github.dchristofolli.dropbox.v1.file.model.FileMapper;
+//import com.github.dchristofolli.dropbox.v1.file.model.FileModelRequest;
+//import com.github.dchristofolli.dropbox.v1.file.service.FileService;
+//import com.github.dchristofolli.dropbox.v1.user.service.UserService;
+//import lombok.AllArgsConstructor;
+//import org.springframework.data.domain.Page;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//@Component
+//@AllArgsConstructor
+//public class FileFacade {
+//    private FileService fileService;
+//    private UserService userService;
+//
+//    public void sendFile(MultipartFile file, String userId) {
+//        fileService.send(file, userService.findById(userId));
+//    }
+//
+//    public void delete(String idUser, String fileName) {
+//        fileService.delete(userService.findById(idUser), fileName);
+//    }
+//
+//    public Page<FileMapper> pagedList(FileModelRequest request) {
+//        return fileService.pagedList(request);
+//    }
+//
+//    public Page<FileMapper> listsSharedWithMe(FileModelRequest request) {
+//        return fileService.listsSharedWithMe(request);
+//    }
+//
+//    public void download(String id, String file) {
+//        fileService.download(id, file);
+//    }
+//
+//    public void downloadSharedWithMe(String id, String file) {
+//        download(userService.findById(id).getFollower(), file);
+//    }
+//
+//}
--- impl/src/main/java/com/github/dchristofolli/dropbox/v1/user/service/UserService.java
+++ impl/src/main/java/com/github/dchristofolli/dropbox/v1/user/service/UserService.java
@@ -13,53 +13,56 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.github.dchristofolli.dropbox.v1.user.mapper.UserMapperImpl.mapToEntity;
+import static com.github.dchristofolli.dropbox.v1.user.mapper.UserMapperImpl.mapToModel;
+
 @Service
 @AllArgsConstructor
 public class UserService {
 
     private UserRepository userRepository;
 
-    public List<UserModel> findAll() {
-        List<UserModel> users = userRepository
-                .findAll()
-                .stream()
-                .map(this::mapToModel)
-                .collect(Collectors.toList());
-        if (users.isEmpty()) throw new ApiException("Nenhum usuário encontrado", HttpStatus.NOT_FOUND);
-        return users;
-    }
+//    public List<UserModel> findAll() {
+//        List<UserModel> users = userRepository
+//                .findAll()
+//                .stream()
+//                .map(this::mapToModel)
+//                .collect(Collectors.toList());
+//        if (users.isEmpty()) throw new ApiException("Nenhum usuário encontrado", HttpStatus.NOT_FOUND);
+//        return users;
+//    }
 
-    public UserModel findById(String id) {
-        return mapToModel(userRepository.findById(id)
-                .orElseThrow(() -> new ApiException("Usuário não encontrado", HttpStatus.NOT_FOUND)));
-    }
+//    public UserModel findById(String id) {
+//        return mapToModel(userRepository.findById(id)
+//                .orElseThrow(() -> new ApiException("Usuário não encontrado", HttpStatus.NOT_FOUND)));
+//    }
 
     public UserModel createUser(UserModel user) {
         FtpUser.saveUser(user.getName().toLowerCase(), user.getPassword());
-        return mapToModel(userRepository.save(UserMapperImpl.mapToEntity(user)));
+        return mapToModel(userRepository.createUser(mapToEntity(user)));
     }
 
-    public UserModel updateUser(UserModel user) {
-        return mapToModel(userRepository.save(UserMapperImpl.mapToEntity(user)));
-    }
-
-    public void deleteUser(String id) {
-        if ((!userRepository.existsById(id))){
-            throw new ApiException("Digite um usuário válido", HttpStatus.BAD_REQUEST);
-        }
-        userRepository.deleteById(id);
-    }
-
-    public UserModel allowFollower(String userId, String followerId) {
-        return userRepository.findById(userId)
-                .map(user -> {
-                    user.setFollower(followerId);
-                    return updateUser(mapToModel(user));
-                }).orElseThrow(() -> new ApiException("Não encontrado", HttpStatus.NOT_FOUND));
-    }
-
-    private UserModel mapToModel(UserEntity user) {
-        return UserMapperImpl.mapToModel(user);
-    }
+//    public UserModel updateUser(UserModel user) {
+//        return mapToModel(userRepository.save(UserMapperImpl.mapToEntity(user)));
+//    }
+//
+//    public void deleteUser(String id) {
+//        if ((!userRepository.existsById(id))){
+//            throw new ApiException("Digite um usuário válido", HttpStatus.BAD_REQUEST);
+//        }
+//        userRepository.deleteById(id);
+//    }
+//
+//    public UserModel allowFollower(String userId, String followerId) {
+//        return userRepository.findById(userId)
+//                .map(user -> {
+//                    user.setFollower(followerId);
+//                    return updateUser(mapToModel(user));
+//                }).orElseThrow(() -> new ApiException("Não encontrado", HttpStatus.NOT_FOUND));
+//    }
+//
+//    private UserModel mapToModel(UserEntity user) {
+//        return UserMapperImpl.mapToModel(user);
+//    }
 
 }
