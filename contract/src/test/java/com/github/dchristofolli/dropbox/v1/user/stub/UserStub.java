d61743907f3da87f28a52e0d76cbcf2b277c0da7
dchristofolli <danielchristofolli@gmail.com>
2019-11-19 12:23:07 -0300

    ajustes no sonarqube

--- contract/src/test/java/com/github/dchristofolli/dropbox/v1/user/stub/UserStub.java
+++ contract/src/test/java/com/github/dchristofolli/dropbox/v1/user/stub/UserStub.java
@@ -1,24 +1,14 @@
 package com.github.dchristofolli.dropbox.v1.user.stub;
 
 import com.github.dchristofolli.dropbox.v1.user.repository.UserEntity;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-@Data
-@Builder
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserStub {
-    public static UserEntity userEntityStub() {
+    public static UserEntity userEntityStub(){
         return UserEntity.builder()
                 .id("13454")
-                .cpf("75949011007")
-                .email("teste@123.com")
-                .follower(null)
-                .password("123456")
-                .name("Bruce")
+                .name("Daniel")
+                .cpf("05553232694")
+                .email("daniel@gmail.com")
                 .build();
     }
 }
--- impl/src/test/java/com/github/dchristofolli/dropbox/v1/user/UserFacadeTest.java
+++
@@ -1,7 +0,0 @@
-package com.github.dchristofolli.dropbox.v1.user;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserFacadeTest {
-
-}
