7d21d40e0e7d7de41eecd8877b14e259f3f52eb4
dchristofolli <danielchristofolli@gmail.com>
2019-11-19 20:46:54 -0300

    ajuste nos testes

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:11
-LABEL AUTHOR="Daniel Christofolli"
-WORKDIR /var/www
-EXPOSE  8080:8080 2021:2021
-ENTRYPOINT ["java","-jar","ftpserver-0.0.1-SNAPSHOT.jar"]
--- impl/src/test/java/com/github/dchristofolli/dropbox/v1/user/service/UserServiceTest.java
+++ impl/src/test/java/com/github/dchristofolli/dropbox/v1/user/service/UserServiceTest.java
@@ -1,53 +1,54 @@
-//package com.github.dchristofolli.dropbox.v1.user.service;
-//
-//import com.github.dchristofolli.dropbox.v1.user.repository.UserRepository;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.rules.ExpectedException;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//
-//import java.util.Collections;
-//
-//import static org.mockito.Mockito.when;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class UserServiceTest {
-//    @Rule
-//    public ExpectedException expectedException = ExpectedException.none();
-//    @Mock
-//    UserRepository repository;
-//    @InjectMocks
-//    UserService service;
+package com.github.dchristofolli.dropbox.v1.user.service;
+
+import com.github.dchristofolli.dropbox.v1.exception.ApiException;
+import com.github.dchristofolli.dropbox.v1.user.repository.UserRepository;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+    @Mock
+    UserRepository repository;
+    @InjectMocks
+    UserService service;
+
+    @Test
+    public void findAll_returns_api_exception_when_the_list_is_empty() {
+        expectedException.expect(ApiException.class);
+        expectedException.expectMessage("Nenhum usuário encontrado");
+        when(repository.findAll()).thenReturn(Collections.emptyList());
+        service.findAll();
+    }
 //
 //    @Test
-//    public void findAll_returns_api_exception_when_the_list_is_empty() {
-//        expectedException.expect(NullPointerException.class);
-//        expectedException.expectMessage("Nenhum usuário encontrado");
-//        when(repository.findAll()).thenReturn(Collections.emptyList());
-//        service.findAll();
+//    void findById() {
+//
 //    }
-////
-////    @Test
-////    void findById() {
-////
-////    }
-////
-////    @Test
-////    void createUser() {
-////    }
-////
-////    @Test
-////    void updateUser() {
-////    }
-////
-////    @Test
-////    void deleteUser() {
-////    }
-////
-////    @Test
-////    void allowFollower() {
-////    }
-//}
+//
+//    @Test
+//    void createUser() {
+//    }
+//
+//    @Test
+//    void updateUser() {
+//    }
+//
+//    @Test
+//    void deleteUser() {
+//    }
+//
+//    @Test
+//    void allowFollower() {
+//    }
+}
